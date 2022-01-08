e0e9ad1d17b3c2c3c7a25509c3dc542493163cbb
carokrny <carolynkrny@gmail.com>
2022-01-07 21:29:10 -0500

    Update pool to cast date type as a string

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,14 @@
 const { Pool, types } = require('pg');
 const devConfig = require('./devConfig');
 
+// cast numeric (OID 1700) as float (string is default in node-postgres)
 types.setTypeParser(1700, function(val) {
     return parseFloat(val);
+});
+
+// cast date (OID 1082) as string (Date object with timestamp is default in node-postgres)
+types.setTypeParser(1082, function(stringValue) {
+  return stringValue; 
 });
 
 // instantiate pool 
